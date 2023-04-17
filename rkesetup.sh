96c61639b288c84cf2e889ac86ca337771dcf352
Michael Cabalin <mike.cabalin@gmail.com>
2023-04-17 12:27:12 +0800

    Update rkesetup.sh

--- rkesetup.sh
+++ rkesetup.sh
@@ -39,7 +39,6 @@ sleep 5s
 #Install Docker and Containerd
 
 {
-{
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
