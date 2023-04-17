df6ee07bb5320b7bb625d24b96669842750acf26
Michael Cabalin <mike.cabalin@gmail.com>
2023-04-17 12:26:44 +0800

    Update rkesetup.sh

--- rkesetup.sh
+++ rkesetup.sh
@@ -39,13 +39,14 @@ sleep 5s
 #Install Docker and Containerd
 
 {
+{
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce=5:20.10.24~3-0~ubuntu-focal containerd.io docker-ce-cli
+apt-cache policy docker-ce 
+sudo apt install -y containerd.io docker-ce=5:20.10.24~3-0~ubuntu-focal docker-ce-cli=5:20.10.24~3-0~ubuntu-focal docker-ce-rootless-extras=5:20.10.24~3-0~ubuntu-focal 
 }
 
 #Setting docker parameters
